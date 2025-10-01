710b7a2c7f295ee3d3e892472189ffd2c03c8bb8
Degra02 <filippodegrandi02@gmail.com>
2025-10-01 13:13:54 +0200

    more idiomatic rust

--- sha3/src/lib.rs
+++ sha3/src/lib.rs
@@ -21,7 +21,8 @@ mod tests {
 
     #[test]
     fn solution() {
-        let to_encode: &str = "FLAG{the_curse_of_the_hex_is_broken_the_door_of_the_crypt_is_now_open}";
+        let to_encode: &str =
+            "FLAG{the_curse_of_the_hex_is_broken_the_door_of_the_crypt_is_now_open}";
         let mut hasher = SHA3::new(ShaVariant::SHA3_224);
         hasher.update(to_encode.as_bytes());
 
--- sha3/src/sha3.rs
+++ sha3/src/sha3.rs
@@ -1,10 +1,5 @@
-#![allow(dead_code)]
-#![allow(renamed_and_removed_lints)]
-
 use std::fmt::{Display, LowerHex};
-
 use zeroize::{Zeroize, ZeroizeOnDrop};
-
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Digest(Vec<u8>);
@@ -58,7 +53,7 @@ pub struct SHA3(Keccak);
 impl SHA3 {
     pub fn new(variant: ShaVariant) -> Self {
         let (rate, capacity, output_length) = variant.parameters();
-        SHA3(Keccak::new(rate/8, capacity/8, output_length/8))
+        SHA3(Keccak::new(rate / 8, capacity / 8, output_length / 8))
     }
 
     pub fn update(&mut self, input: &[u8]) {
@@ -75,14 +70,30 @@ impl SHA3 {
 }
 
 const RHO: [u64; 24] = [
-    0x0000000000000001, 0x0000000000008082, 0x800000000000808A,
-    0x8000000080008000, 0x000000000000808B, 0x0000000080000001,
-    0x8000000080008081, 0x8000000000008009, 0x000000000000008A,
-    0x0000000000000088, 0x0000000080008009, 0x000000008000000A,
-    0x000000008000808B, 0x800000000000008B, 0x8000000000008089,
-    0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
-    0x000000000000800A, 0x800000008000000A, 0x8000000080008081,
-    0x8000000000008080, 0x0000000080000001, 0x8000000080008008,
+    0x0000000000000001,
+    0x0000000000008082,
+    0x800000000000808A,
+    0x8000000080008000,
+    0x000000000000808B,
+    0x0000000080000001,
+    0x8000000080008081,
+    0x8000000000008009,
+    0x000000000000008A,
+    0x0000000000000088,
+    0x0000000080008009,
+    0x000000008000000A,
+    0x000000008000808B,
+    0x800000000000008B,
+    0x8000000000008089,
+    0x8000000000008003,
+    0x8000000000008002,
+    0x8000000000000080,
+    0x000000000000800A,
+    0x800000008000000A,
+    0x8000000080008081,
+    0x8000000000008080,
+    0x0000000080000001,
+    0x8000000080008008,
 ];
 
 const PI: [[u32; 5]; 5] = [
@@ -92,7 +103,6 @@ const PI: [[u32; 5]; 5] = [
     [28, 55, 25, 21, 56],
     [27, 20, 39, 8, 14],
 ];
-
 
 #[derive(Zeroize, ZeroizeOnDrop, Debug)]
 struct Keccak {
@@ -163,7 +173,6 @@ impl Keccak {
         let final_block: Vec<u8> = self.buffer.drain(..).collect();
         self.absorb_block(&final_block);
 
-
         let mut output = Vec::new();
         while output.len() < self.output_length {
             let num_lanes = self.rate / 8;
@@ -204,7 +213,6 @@ impl Keccak {
         }
     }
 
-
     /// Keccak-f[1600] permutation
     fn keccak_f(&mut self) {
         for round in 0..24 {
@@ -222,19 +230,23 @@ impl Keccak {
 
     fn theta(&mut self) {
         let mut c = [0u64; 5];
-        for x in 0..5 {
-            c[x] = self.state[x] ^ self.state[x + 5] ^ self.state[x + 10] ^ self.state[x + 15] ^ self.state[x + 20];
-        }
 
+        for (x, cx) in c.iter_mut().enumerate() {
+            *cx = self.state[x]
+                ^ self.state[x + 5]
+                ^ self.state[x + 10]
+                ^ self.state[x + 15]
+                ^ self.state[x + 20];
+        }
 
         let mut d = [0u64; 5];
         for x in 0..5 {
             d[x] = c[(x + 4) % 5] ^ c[(x + 1) % 5].rotate_left(1);
         }
 
-        for x in 0..5 {
+        for (x, &dx) in d.iter().enumerate() {
             for y in 0..5 {
-                self.state[x + 5 * y] ^= d[x];
+                self.state[x + 5 * y] ^= dx;
             }
         }
     }
@@ -253,7 +265,8 @@ impl Keccak {
         let mut b = [0u64; 25];
         for x in 0..5 {
             for y in 0..5 {
-                b[x + 5 * y] = self.state[x + 5 * y] ^ ((!self.state[((x + 1) % 5) + 5 * y]) & self.state[((x + 2) % 5) + 5 * y]);
+                b[x + 5 * y] = self.state[x + 5 * y]
+                    ^ ((!self.state[((x + 1) % 5) + 5 * y]) & self.state[((x + 2) % 5) + 5 * y]);
             }
         }
         self.state = b;
