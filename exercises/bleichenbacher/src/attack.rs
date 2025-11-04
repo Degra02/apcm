629a5974e771fd3dae19658efcdc0f172cb19a15
Degra02 <filippodegrandi02@gmail.com>
2025-11-04 21:12:45 +0100

    changes

--- exercises/bleichenbacher/src/attack.rs
+++ exercises/bleichenbacher/src/attack.rs
@@ -1,12 +1,9 @@
 #![allow(dead_code)]
 #![allow(non_snake_case)]
 
-use std::u128;
 
-use hex::ToHex;
 use num_bigint::BigUint;
-use num_traits::{Num, Zero};
-use rand::Rng;
+use num_traits::{One, Zero};
 use reqwest::blocking::Client;
 use serde_json::Value;
 
@@ -18,7 +15,6 @@ pub struct Attacker {
     pub url: String,
     pub c_bytes: Vec<u8>,
     pub state: AttackState,
-    pub timing_threshold: u128,
 }
 
 impl Attacker {
@@ -48,51 +44,12 @@ impl Attacker {
 
         let state = AttackState::new(rsa_pubkey, &c);
 
-        let timing_threshold = 0u128;
-
         Ok(Self {
             client,
             url: String::from(url),
             c_bytes: c,
             state,
-            timing_threshold,
         })
-    }
-
-    pub fn calibrate_timing(&self, repeats: u32) -> Result<u128, CustomError> {
-        let trials = 5usize;
-        let mut valid_times = Vec::new();
-        for _ in 0..trials {
-            let res = self.decrypt(&self.c_bytes, Some(repeats))?;
-            valid_times.push(res.time_ns);
-        }
-        let valid_mean = valid_times.iter().copied().sum::<u128>() / (valid_times.len() as u128);
-
-        // measure several invalid ciphertexts (flip one byte or random)
-        let mut invalid_times = Vec::new();
-        for _ in 0..trials {
-            let mut rng = rand::rng();
-            let mut bad = self.c_bytes.clone();
-
-            let idx = rng.random_range(0..bad.len());
-            bad[idx] ^= 0xff;
-            let res = self.decrypt(&bad, Some(repeats))?;
-            invalid_times.push(res.time_ns);
-        }
-        let invalid_mean = invalid_times.iter().copied().sum::<u128>() / (invalid_times.len() as u128);
-
-        println!("calibrate: valid_mean={} ns, invalid_mean={} ns", valid_mean, invalid_mean);
-
-        // choose midpoint as threshold
-        let threshold = (valid_mean + invalid_mean) / 2u128;
-        Ok(threshold)
-    }
-
-    pub fn is_valid_by_timing(&self, cipher: &[u8], repeats: u32) -> Result<bool, CustomError> {
-        let res = self.decrypt(cipher, Some(repeats))?;
-        let t = res.time_ns;
-        let threshold = self.timing_threshold;
-        Ok(t < threshold)
     }
 
     pub fn encrypt(&self, plain: &[u8], repeat: Option<u32>) -> Result<EncryptRes, CustomError> {
@@ -124,17 +81,15 @@ impl Attacker {
         Ok(res)
     }
 
-    /// Step 1 can be skipped since `c` is already a valid PKCS#1 v1.5 ciphertext.
+    /// Step 2a can be skipped since `c` is already a valid PKCS#1 v1.5 ciphertext.
     pub fn bleichenbacher_attack(&mut self) -> Result<Vec<u8>, CustomError> {
-        println!("Calibrating timing");
-        self.timing_threshold = self.calibrate_timing(1000)?;
-        println!("Timing threshold set to {} ns", self.timing_threshold);
 
-        let mut si = self.step2a()?;
+        // let mut si = self.step2a()?;
+        let mut si = BigUint::one();
 
-        let mut it = 0u64;
+        let mut it = 1u64;
         loop {
-            println!("Iteration {}: M = {:?}, si = {}", it, self.state.M, si);
+            println!("Iteration {}, si = {}, M.len(): {}", it, si, self.state.M.len());
             it += 1;
 
             if self.state.M.len() >= 2 {
@@ -166,61 +121,90 @@ impl Attacker {
     }
 
     fn step2a(&mut self) -> Result<BigUint, CustomError> {
-        // let mut s = (&self.state.n + 3u8 * &self.state.B - 1u8) / (3u8 * &self.state.B);
-        let mut s = div_ceil(&self.state.n, &(3u8 * &self.state.B));
+        let b3 = BigUint::from(3u8) * &self.state.B;
+        let mut s = div_ceil(&self.state.n, &b3);
+
         loop {
             let c_prime = (&self.state.c * s.modpow(&self.state.e, &self.state.n)) % &self.state.n;
             let c_prime_bytes = to_k_bytes_be(&c_prime, self.state.k);
 
-            println!("Trying c_prime: {}", hex::encode(&c_prime_bytes));
-            if self.is_valid_by_timing(&c_prime_bytes, 1000)? {
+            if self.decrypt(&c_prime_bytes, None)?.is_valid_pkcs1() {
                 return Ok(s);
             }
-            s += 1u8;
+            s += BigUint::one();
         }
     }
 
+
     fn step2b(&mut self, prev_s: &BigUint) -> Result<BigUint, CustomError> {
-        let mut si = prev_s + 1u8;
+        let mut si = prev_s + BigUint::one();
         loop {
             let c_prime = (&self.state.c * si.modpow(&self.state.e, &self.state.n)) % &self.state.n;
             let c_prime_bytes = to_k_bytes_be(&c_prime, self.state.k);
-            if self.is_valid_by_timing(&c_prime_bytes, 1000)? {
+
+            if self.decrypt(&c_prime_bytes, None)?.is_valid_pkcs1() {
                 return Ok(si);
             }
-            si += 1u8;
+            si += BigUint::one();
         }
     }
 
     fn step2c(&mut self, prev_s: &BigUint) -> Result<BigUint, CustomError> {
-        let (a, b) = &self.state.M[0].clone();
+        let (a, b) = &self.state.M[0];
+        let one = BigUint::from(1u8);
+        let two = BigUint::from(2u8);
+        let three = BigUint::from(3u8);
 
-        let mut ri = 2u8 * (prev_s * b - 2u8 * &self.state.B) / &self.state.n;
+        // let mut ri = 2u8 * (prev_s * b - 2u8 * &self.state.B) / &self.state.n;
+        // let mut ri = div_ceil(&( 2u8 * (prev_s * b - 2u8 * &self.state.B) ), &self.state.n);
+        let num = &two * (prev_s * b - &two * &self.state.B);
+        let mut ri = div_ceil(&num, &self.state.n);
+
         loop {
-            let mut si = (&self.state.B + &ri * &self.state.n) / b;
+            // si = ceil((B + ri*n) / b)
+            let mut si = div_ceil(&(&two * &self.state.B + &ri * &self.state.n), b);
+            // s_end = floor((3B + ri*n) / a)
+            let s_end = div_floor(&(&three * &self.state.B - &one + &ri * &self.state.n), a);
 
-            while si < (3u8 * &self.state.B + &ri * &self.state.n) / a {
+            println!("si: {}, s_end: {}", si, s_end);
+
+            while si <= s_end {
+                println!("Testing si = {}", si);
                 let c_prime =
                     (&self.state.c * si.modpow(&self.state.e, &self.state.n)) % &self.state.n;
                 let c_prime_bytes = to_k_bytes_be(&c_prime, self.state.k);
 
-                if self.is_valid_by_timing(&c_prime_bytes, 1000)? {
+                if self.decrypt(&c_prime_bytes, None)?.is_valid_pkcs1() {
                     return Ok(si);
                 }
-                si += 1u8;
+                si += BigUint::one();
             }
 
-            ri += 1u8;
+            ri += BigUint::one();
         }
     }
 
     fn step3(&mut self, si: &BigUint) {
-        let mut new_M = Vec::<(BigUint, BigUint)>::new();
+        let one = BigUint::from(1u8);
+        let two = BigUint::from(2u8);
+        let three = BigUint::from(3u8);
+
+        let mut new_M = Vec::<Interval>::new();
 
         for (a, b) in &self.state.M {
-            let r_lower = div_ceil(&(a * si - 3u8 * &self.state.B), &self.state.n);
+            let numerator = {
+                let left = a * si;
+                let three_b_minus_1 = &three * &self.state.B - &one;
+                clamp_sub(&left, &three_b_minus_1)
+            };
+            let r_lower = div_ceil(&numerator, &self.state.n);
+            // let r_lower = div_ceil(&(a * si - 3u8 * &self.state.B), &self.state.n);
             // let r_upper = (b * si - 2u8 * &self.state.B) / &self.state.n;
-            let r_upper = div_ceil(&(b * si - 2u8 * &self.state.B), &self.state.n);
+            // let r_upper = div_ceil(&(b * si - 2u8 * &self.state.B), &self.state.n);
+
+            // r_upper = floor((b*si - 2B) / n) but clamp if negative
+            let numerator_up = clamp_sub(&(b * si), &(&two * &self.state.B));
+            let r_upper = div_floor(&numerator_up, &self.state.n);
 
             let mut r = r_lower.clone();
             while r <= r_upper {
@@ -236,7 +220,7 @@ impl Attacker {
                 if lower_bound <= upper_bound {
                     new_M.push((lower_bound, upper_bound));
                 }
-                r += 1u8;
+                r += BigUint::one();
             }
         }
 
@@ -256,28 +240,51 @@ impl Attacker {
 pub fn div_ceil(num: &BigUint, den: &BigUint) -> BigUint {
     let quot = num / den;
     let rem = num % den;
-    if rem.is_zero() {
-        quot
-    } else {
-        quot + 1u8
-    }
+    if rem.is_zero() { quot } else { quot + BigUint::one() }
 }
 
-fn to_k_bytes_be(x: &BigUint, k: usize) -> Vec<u8> {
-    // let mut x_bytes = x.to_bytes_be();
-    // while x_bytes.len() < k {
-    //     x_bytes.insert(0, 0u8);
-    // }
-    // x_bytes
-    let x_bytes = x.to_bytes_be();
-    if x_bytes.len() < k {
-        let mut padded = vec![0u8; k - x_bytes.len()];
-        padded.extend_from_slice(&x_bytes);
-        padded
-    } else {
-        x_bytes
-    }
+fn div_floor(num: &BigUint, den: &BigUint) -> BigUint {
+    num / den
 }
+
+fn clamp_sub(a: &BigUint, b: &BigUint) -> BigUint {
+    if a > b { a - b } else { BigUint::zero() }
+}
+
+
+fn to_k_bytes_be(x: &BigUint, k: usize) -> Vec<u8> {
+    let mut x_bytes = x.to_bytes_be();
+    while x_bytes.len() < k {
+        x_bytes.insert(0, 0u8);
+    }
+    x_bytes
+}
+
+pub fn unpad_pkcs1_v15(block: &[u8]) -> Result<Vec<u8>, CustomError> {
+    // Check the fixed header bytes 0x00 0x02
+    // if block[0] != 0x00 || block[1] != 0x02 {
+    //     return Err(CustomError::Other("Invalid PKCS#1 v1.5 padding header".into()));
+    // }
+
+    // Find the 0x00 separator after the padding string PS.
+    // PS must be at least 8 bytes, so scanning starts at index 2 and the
+    // earliest separator index is 2 + 8 = 10.
+    let mut sep_index: Option<usize> = None;
+    for i in 10..block.len() {
+        if block[i] == 0x00 {
+            sep_index = Some(i);
+            break;
+        }
+    }
+
+    let sep_index = sep_index.ok_or_else(|| CustomError::Other("Padding separator 0x00 not found".into()))?;
+
+    // The message starts after the separator
+    let message = block[(sep_index + 1)..].to_vec();
+    Ok(message)
+}
+
+type Interval = (BigUint, BigUint);
 
 #[derive(Debug)]
 pub struct AttackState {
@@ -285,18 +292,16 @@ pub struct AttackState {
     pub n: BigUint,
     pub B: BigUint,
     pub c: BigUint,
-    pub M: Vec<(BigUint, BigUint)>,
+    pub M: Vec<Interval>,
     pub e: BigUint,
 }
 
 impl AttackState {
     pub fn new(rsa_pubkey: PublicKeyInfo, c: &[u8]) -> Self {
-        let k = rsa_pubkey.public_key_size_bits / 8;
-
         let decoded = hex::decode(&rsa_pubkey.public_modulus_hex).expect("Invalid hex in modulus");
+        let k = decoded.len();
         let n = BigUint::from_bytes_be(&decoded);
         let e = BigUint::from(rsa_pubkey.public_exponent_dec);
-
 
         // B = 2^(8*(k-2))
         let B = {
---
+++ exercises/bleichenbacher/src/main.rs
@@ -0,0 +1,33 @@
+use crate::{attack::{unpad_pkcs1_v15, Attacker}, utils::CustomError};
+use hex_literal::hex;
+
+mod attack;
+mod utils;
+
+const URL: &str = "https://medieval-adelle-jonistartuplab-17499dda.koyeb.app";
+const TEST_URL: &str = "http://localhost:8000";
+
+const CIPHERTEXT: [u8; 128] = hex!("2d38aeb156ef11bc165989a12669b30cf20cda8a196288a2a24262c9b43bd715ba76dbd8c42337d4ec0d7d40a77fe4a5f37a5a59e0e5e5506abb588225d5f3483f4f4bde4e3771cec55f12c0dcca56f5d9a3110bc50dc47d7d04db8e4e57044574ca101301c1efc64a497af420b286fe6baf3a4adc883a2ed24956c8eb502817");
+
+fn main() -> Result<(), CustomError> {
+    let mut attacker = Attacker::new(URL, Some(&CIPHERTEXT))?;
+    let res = attacker.bleichenbacher_attack()?;
+    println!("{:?}", res);
+
+    match unpad_pkcs1_v15(&res) {
+        Ok(plaintext) => {
+            println!("Recovered plaintext ({} bytes): {:?}", plaintext.len(), plaintext);
+            // If the original message was printable UTF-8:
+            if let Ok(s) = std::str::from_utf8(&plaintext) {
+                println!("As UTF-8: {}", s);
+            } else {
+                println!("Plaintext is not valid UTF-8; raw bytes shown above.");
+            }
+        }
+        Err(e) => {
+            eprintln!("Unpadding failed: {:?}", e);
+        }
+    }
+
+    Ok(())
+}
