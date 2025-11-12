49911837087ce132e1927fb4397a82e56ac865f4
Degra02 <filippodegrandi02@gmail.com>
2025-11-12 11:59:41 +0100

    changes

--- exercises/pduel/src/eddsa.rs
+++ exercises/pduel/src/eddsa.rs
@@ -1,30 +1,61 @@
-use curve25519_dalek::Scalar;
+use curve25519_dalek::{edwards::CompressedEdwardsY, EdwardsPoint, Scalar};
 use sha2::Digest;
 
+use crate::utils::CustomError;
+
 pub struct SigningKey {
-    pub(crate) secret_key: [u8; 32],
-    pub(crate) verifying_key: [u8; 32],
+    pub(crate) seed: [u8; 32],
+    pub(crate) scalar: Scalar,
+    pub(crate) prefix: [u8; 32],
+    pub(crate) verifying_key: VerifyingKey,
 }
 
 impl SigningKey {
     pub fn generate(seed: &[u8; 32]) -> Self {
         let digest = sha2::Sha512::digest(seed);
-        let mut secret_key = [0u8; 32];
-        secret_key.copy_from_slice(&digest[0..32]);
 
-        secret_key[0] &= 248;
-        secret_key[31] &= 63;
-        secret_key[31] |= 64;
+        let mut scalar_bytes = [0u8; 32];
+        scalar_bytes.copy_from_slice(&digest[0..32]);
+        scalar_bytes[0] &= 248;
+        scalar_bytes[31] &= 63;
+        scalar_bytes[31] |= 64;
 
-        let scalar = Scalar::from_bytes_mod_order(secret_key);
+        let mut prefix = [0u8; 32];
+        prefix.copy_from_slice(&digest[32..64]);
 
-        let verifying_key = (scalar * curve25519_dalek::constants::ED25519_BASEPOINT_POINT)
-            .compress()
-            .to_bytes();
+        let scalar = Scalar::from_bytes_mod_order(scalar_bytes);
+        let verifying_key = VerifyingKey::from_scalar(&scalar);
 
         SigningKey {
-            secret_key,
+            seed: *seed,
+            scalar,
+            prefix,
             verifying_key,
         }
+    }
+
+    pub fn sign(&self, message: &[u8]) -> [u8; 64] {
+        let mut hasher = sha2::Sha512::new();
+        hasher.update(&self.prefix);
+        hasher.update(message);
+        let r_scalar = Scalar::from_hash(hasher);
+
+        let r_pint = &r_sca
+
+
+
+    }
+}
+
+pub struct VerifyingKey {
+    pub(crate) point: EdwardsPoint,
+    pub(crate) compressed: CompressedEdwardsY,
+}
+
+impl VerifyingKey {
+    pub fn from_scalar(scalar: &Scalar) -> Self {
+        let point = scalar * curve25519_dalek::constants::ED25519_BASEPOINT_POINT;
+        let compressed = point.compress();
+        VerifyingKey { point, compressed }
     }
 }
--- exercises/pduel/src/main.rs
+++ exercises/pduel/src/main.rs
@@ -1,9 +1,15 @@
 #![allow(dead_code)]
 
 mod eddsa;
+mod utils;
 
 // Name: Filippo De Grandi
 // Group: curvy
+//
+// Why the crate curve25519-dalek is used in this implementation:
+// - seamless integraiton with sha2 crate
+// - high level abstractions for point and scalar operations
+// - well maintained and widely used in the Rust cryptographic community
 
 fn main() {
     let secret_key = rand::random_iter::<u8>().take(32).collect::<Vec<u8>>();
