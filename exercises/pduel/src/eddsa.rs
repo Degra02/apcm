b6507390bb848e6672e3ead9697f6f8c48b40956
Degra02 <filippodegrandi02@gmail.com>
2025-11-12 11:32:42 +0100

    changes

--- exercises/pduel/src/eddsa.rs
+++ exercises/pduel/src/eddsa.rs
@@ -1,31 +1,30 @@
+use curve25519_dalek::Scalar;
 use sha2::Digest;
 
 pub struct SigningKey {
     pub(crate) secret_key: [u8; 32],
-    pub(crate) verifying_key: Vec<u8>,
+    pub(crate) verifying_key: [u8; 32],
 }
 
 impl SigningKey {
-    pub fn generate() -> Self {
-        let secret_key = rand::random_iter::<u8>().take(32).collect::<Vec<u8>>();
+    pub fn generate(seed: &[u8; 32]) -> Self {
+        let digest = sha2::Sha512::digest(seed);
+        let mut secret_key = [0u8; 32];
+        secret_key.copy_from_slice(&digest[0..32]);
 
-        let hashed_key: [u8; 64] = sha2::Sha512::digest(&secret_key).into();
-        let hashes_key = sha2::Sha512::default().chain_update(secret_key).finalize();
-        let clamped_secred_key = {
-            let mut key = hashed_key[..32].to_owned();
-            key[0] &= 248;
-            key[31] &= 127;
-            key[31] |= 64;
-            key
-        };
+        secret_key[0] &= 248;
+        secret_key[31] &= 63;
+        secret_key[31] |= 64;
 
-        let scalar = u32::from_le_bytes([
-            clamped_secred_key[0],
-            clamped_secred_key[1],
-            clamped_secred_key[2],
-            clamped_secred_key[3],
-        ]);
+        let scalar = Scalar::from_bytes_mod_order(secret_key);
 
+        let verifying_key = (scalar * curve25519_dalek::constants::ED25519_BASEPOINT_POINT)
+            .compress()
+            .to_bytes();
 
+        SigningKey {
+            secret_key,
+            verifying_key,
+        }
     }
 }
--- exercises/pduel/src/main.rs
+++ exercises/pduel/src/main.rs
@@ -1,9 +1,12 @@
+#![allow(dead_code)]
+
 mod eddsa;
 
 // Name: Filippo De Grandi
 // Group: curvy
 
 fn main() {
+    let secret_key = rand::random_iter::<u8>().take(32).collect::<Vec<u8>>();
 }
 
 #[cfg(test)]
